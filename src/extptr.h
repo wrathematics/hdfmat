5f268858bbc1eefbfad2990acb485302b42d995f
wrathematics <wrathematics@gmail.com>
2021-09-22 17:57:20 -0400

    add C-R utils

---
+++ src/extptr.h
@@ -0,0 +1,24 @@
+#ifndef EXTPTR_H
+#define EXTPTR_H
+#pragma once
+
+
+#include <Rinternals.h>
+
+
+#define newRptr(ptr,Rptr,fin) PROTECT(Rptr = R_MakeExternalPtr(ptr, R_NilValue, R_NilValue));R_RegisterCFinalizerEx(Rptr, fin, TRUE)
+#define getRptr(ptr) R_ExternalPtrAddr(ptr)
+
+template <class T>
+static inline void hdf_object_finalizer(SEXP Rptr)
+{
+  if (NULL == getRptr(Rptr))
+    return;
+  
+  T *x = (T*) getRptr(Rptr);
+  delete x;
+  R_ClearExternalPtr(Rptr);
+}
+
+
+#endif
---
+++ src/hdfmat.h
@@ -0,0 +1,15 @@
+#ifndef HDFMAT_H
+#define HDFMAT_H
+#pragma once
+
+
+#include <H5Cpp.h>
+#include <H5DataSpace.h>
+
+#include <R.h>
+#include <Rinternals.h>
+
+#define CHARPT(x,i) ((char*)CHAR(STRING_ELT(x,i)))
+
+
+#endif
