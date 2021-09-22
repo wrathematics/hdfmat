4d87cdb6283910eecc44e99a13e0f8658dca28b8
wrathematics <wrathematics@gmail.com>
2021-09-22 17:56:21 -0400

    first

---
+++ R/hdfmat-package.r
@@ -0,0 +1,10 @@
+#' HDF5 Matrix Operations
+#' 
+#' @description
+#' Some out-of-core matrix operations via HDF5.
+#' 
+#' @docType package
+#' @name hdfmat-package
+#' @author Drew Schmidt
+#' @keywords package
+NULL
