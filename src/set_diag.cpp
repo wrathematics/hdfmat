96f91d7b143e93e16ceda640899c313f064972bf
wrathematics <wrathematics@gmail.com>
2021-09-22 17:58:27 -0400

    add set_diag

---
+++ src/set_diag.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+
+#include "hdfmat.h"
+#include "extptr.h"
+
+
+template <typename T>
+static inline void set_diag(const T val, const hsize_t m, const hsize_t n,
+  H5::DataSet *dataset, H5::PredType h5type)
+{
+  const hsize_t len = m<n?m:n;
+  
+  hsize_t slice[2];
+  slice[0] = 1;
+  slice[1] = 1;
+  
+  H5::DataSpace mem_space(2, slice, NULL);
+  H5::DataSpace data_space = dataset->getSpace();
+  
+  hsize_t offset[2];
+  
+  for (hsize_t i=0; i<len; i++)
+  {
+    offset[0] = i;
+    offset[1] = i;
+    data_space.selectHyperslab(H5S_SELECT_SET, slice, offset);
+    
+    dataset->write(&val, h5type, mem_space, data_space);
+  }
+}
+
+
+
+extern "C" SEXP R_hdfmat_set_diag(SEXP m_, SEXP n_, SEXP ds, SEXP val_)
+{
+  // H5::Exception::dontPrint();
+  H5::DataSet *dataset = (H5::DataSet*) getRptr(ds);
+  
+  const hsize_t m = (hsize_t) REAL(m_)[0];
+  const hsize_t n = (hsize_t) REAL(n_)[0];
+  const double val = REAL(val_)[0];
+  
+  set_diag(val, m, n, dataset, H5::PredType::IEEE_F64LE);
+  
+  return R_NilValue;
+}
