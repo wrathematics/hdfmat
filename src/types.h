8b8c1a75b34318720e9457ce3398fce56d598942
wrathematics <wrathematics@gmail.com>
2021-09-24 20:52:35 -0400

    add types.h

---
+++ src/types.h
@@ -0,0 +1,15 @@
+#ifndef HDFMAT_TYPES_H
+#define HDFMAT_TYPES_H
+#pragma once
+
+
+// -----------------------------------------------------------------------------
+// types mapping - must match R/00-globals.r
+// -----------------------------------------------------------------------------
+
+#define TYPE_DOUBLE 1
+#define TYPE_FLOAT 2
+#define TYPE_ERR "unsupported fundamental type"
+
+
+#endif
