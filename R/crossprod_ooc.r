6d715573ecead97d56536d3a83251c79631da642
wrathematics <wrathematics@gmail.com>
2021-10-18 12:25:45 -0400

    add compression, better type support to crossprod

--- R/crossprod_ooc.r
+++ R/crossprod_ooc.r
@@ -8,24 +8,33 @@
 #' Name of the file to use for the out-of-core storage.
 #' @param name
 #' The dataset name within the HDF5 file.
+#' @param compression The compression level, an integer from 0 (no compression)
+#' to 9 (highest compression). Run-time performance degrades with increased
+#' compression levels.
 #' 
 #' @return Returns an hdfmat object.
 #' 
 #' @rdname crossprod_ooc
 #' @export
-crossprod_ooc = function(x, file, name="crossprod")
+crossprod_ooc = function(x, file, name="crossprod", compression=0L)
 {
   if (!is.matrix(x) && !float::is.float(x))
     x = as.matrix(x)
   
   if (float::is.float(x))
+  {
     x = x@Data
+    type = "float"
+  }
   else if (!is.double(x))
+  {
     storage.mode(x) = "double"
+    type = "double"
+  }
   
   n = as.double(ncol(x))
   
-  h = hdfmat(file, name, n, n, "double")
+  h = hdfmat(file, name, n, n, type, compression=compression)
   h$fill_crossprod(x)
   
   h
@@ -35,19 +44,25 @@ crossprod_ooc = function(x, file, name="crossprod")
 
 #' @rdname crossprod_ooc
 #' @export
-tcrossprod_ooc = function(x, file, name="tcrossprod")
+tcrossprod_ooc = function(x, file, name="tcrossprod", compression=0L)
 {
   if (!is.matrix(x) && !float::is.float(x))
     x = as.matrix(x)
   
   if (float::is.float(x))
+  {
     x = x@Data
+    type = "float"
+  }
   else if (!is.double(x))
+  {
     storage.mode(x) = "double"
+    type = "double"
+  }
   
   m = as.double(nrow(x))
   
-  h = hdfmat(file, name, m, m, "double")
+  h = hdfmat(file, name, m, m, type, compression=compression)
   h$fill_tcrossprod(x)
   
   h
